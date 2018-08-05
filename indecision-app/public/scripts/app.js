25e5b324a93fe978fc45c0edcd6746cace39c85c
Tod Fleeman <rtoddf@me.com>
2018-08-05 14:57:17 -0400

    back to the other js file

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,30 +1,82 @@
 'use strict';
 
-function square(x) {
-	return x * x;
+// JSX - Javascript XML
+var course = {
+	name: 'This is JSX from app.js',
+	subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor imperdiet leo at iaculis. Duis pharetra blandit nisi, eget varius mauris auctor ac.',
+	options: ['item 01', 'item 02']
+};
+
+var templateOne = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		course.name
+	),
+	course.subtitle && React.createElement(
+		'p',
+		null,
+		course.subtitle
+	),
+	React.createElement(
+		'p',
+		null,
+		course.options.length > 0 ? 'you have options' : 'you have no options'
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			course.options[0]
+		),
+		React.createElement(
+			'li',
+			null,
+			course.options[1]
+		)
+	)
+);
+
+var user = {
+	name: 'Larry Davis',
+	age: 54,
+	location: 'Dekalb'
+};
+
+function getLocation(location) {
+	return location ? React.createElement(
+		'p',
+		null,
+		'Location: ',
+		location
+	) : undefined;
 }
 
-console.log(square(3));
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'name: ',
+		user.name ? user.name : 'anonymous'
+	),
+	user.age && user.age >= 18 && React.createElement(
+		'p',
+		null,
+		'age: ',
+		user.age
+	),
+	getLocation(user.location)
+);
 
-// arrow functions are anonumous - they can't be named
-// they have to be assigned to const or let
-var cube = function cube(x) {
-	return x * x * x;
-};
+var appRoot01 = document.getElementById('example01');
+var appRoot02 = document.getElementById('example02');
 
-// the more concise version can't be used ALL the time, but can be used for simple returns
-var cubed = function cubed(x) {
-	return x * x * x;
-};
-
-console.log(cube(3));
-console.log(cubed(4));
-
-var getFirstName = function getFirstName(name) {
-	return name.split(" ")[0];
-};
-console.log(getFirstName('Todd Fleeman'));
-
-function boob() {
-	document.getElementById('bob');
-}
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(templateOne, appRoot01);
+ReactDOM.render(templateTwo, appRoot02);
